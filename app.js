39c1f29b4961a5620225f4f06739142b74281b32
23110089 <152385695+23110089@users.noreply.github.com>
2024-10-05 05:00:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ document.getElementById("message-input").addEventListener("keypress", (event) =>
 
 // đăng xuất
 document.getElementById("logout-button").addEventListener("click", () => {
-    document.cookie = `path=/`;
+    document.cookie = "path=/";
     location.reload();
 });
 
